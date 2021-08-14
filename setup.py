44aad9fa6c3477d145e2bdd76edbbe79eec10267
Meng-Xiang-Rui <mxr@mail.ustc.edu.cn>
2021-08-14 14:26:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="qusource",
-  version="0.0.3",
+  version="0.0.4",
   author="Xiangrui Meng",
   author_email="mxr@mail.ustc.edu.cn",
   description="Quantum circuit simulator",
